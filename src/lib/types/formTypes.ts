ecaf4325595e6fe94a88e1c047c455b000c33eac
codewithashim <codewithashim@gmail.com>
2025-03-25 22:30:32 +0600

    Form Schema and type is updatee

---
+++ src/lib/types/formTypes.ts
@@ -0,0 +1,11 @@
+export interface FormData {
+  fullName: string;
+  email: string;
+  phone: string;
+  street: string;
+  city: string;
+  zipCode: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+}
